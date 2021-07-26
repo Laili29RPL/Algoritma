a88c9015a4ca942cd561f40a9ce299493be477a8
LailiRachma <lailirachma4@gmail.com>
2021-07-26 14:35:04 +0700

    upload project

---
+++ src/aritmatika3/Aritmatika3.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmatika3;
+public class Aritmatika3 {
+    public static void main(String[] args) {
+      float bil_a, bil_b, hasil;
+       bil_a = (float) 454.55;
+       bil_b = (float) 546.55;
+       hasil=bil_a + bil_b;
+        System.out.println("Hasil Penjumlahan kedua bilangan: ");
+        System.out.println("hasil= " +hasil);
+    }
+    
+}
